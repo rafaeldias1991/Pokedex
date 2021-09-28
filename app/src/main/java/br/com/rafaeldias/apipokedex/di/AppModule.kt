6fcee0b868fdfa59a774a6f62d6421691011e7ed
Giovanni Melo <web.gcm@gmail.com>
2021-09-27 22:13:34 -0300

    Added new files from feature

--- app/src/main/java/br/com/rafaeldias/apipokedex/di/AppModule.kt
+++ app/src/main/java/br/com/rafaeldias/apipokedex/di/AppModule.kt
@@ -1,28 +1,39 @@
 package br.com.rafaeldias.apipokedex.di
 
 import br.com.rafaeldias.apipokedex.adapter.PokemonAdapter
+import br.com.rafaeldias.apipokedex.api.PokedexApi
+import br.com.rafaeldias.apipokedex.api.ServiceClient
 import br.com.rafaeldias.apipokedex.api.repository.ItemDaoRepository
+import br.com.rafaeldias.apipokedex.api.repository.PokedexRepository
 import br.com.rafaeldias.apipokedex.ui.detail.DatailsViewModel
 import br.com.rafaeldias.apipokedex.ui.home.HomeViewModel
 import br.com.rafaeldias.apipokedex.usecase.ApplySearchFilter
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
+val pokedexModule = module {
+    single { ServiceClient.getServiceClient(apiClass = PokedexApi::class.java) }
+
+    factory { PokedexRepository(api = get()) }
+}
+
 val viewModelModule = module {
 
-    viewModel { HomeViewModel(get())}
+    viewModel { HomeViewModel(idaov = get(), pokedexRepository = get())}
     viewModel { DatailsViewModel(get())}
 
 
 }
-val useCaseModule = module {
+private val useCaseModule = module {
     factory { PokemonAdapter() }
-    factory <ApplySearchFilter>{ ApplySearchFilter() }
+    factory { ApplySearchFilter() }
 
 }
 
-val usedao = module {
+private val useDao = module {
     single { ItemDaoRepository() }
 }
+
+fun getAppModules() = listOf(viewModelModule, useCaseModule, useDao, pokedexModule)
 
 
--- app/src/main/java/br/com/rafaeldias/apipokedex/ui/home/HomeViewModel.kt
+++ app/src/main/java/br/com/rafaeldias/apipokedex/ui/home/HomeViewModel.kt
@@ -2,14 +2,18 @@ package br.com.rafaeldias.apipokedex.ui.home
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.liveData
 import androidx.lifecycle.viewModelScope
 import br.com.rafaeldias.apipokedex.api.repository.ItemDaoRepository
+import br.com.rafaeldias.apipokedex.api.repository.PokedexRepository
 import br.com.rafaeldias.apipokedex.domain.Pokemon
+import br.com.rafaeldias.apipokedex.utils.State
+import java.lang.Exception
 
 
 class HomeViewModel(
     private val idaov: ItemDaoRepository,
-
+    private val pokedexRepository: PokedexRepository
 ) : ViewModel() {
     var pokemonData = MutableLiveData<List<Pokemon>>()
 
@@ -23,12 +27,19 @@ class HomeViewModel(
        idaov.callListApi()
     }
 
-
-
-
-
-
-
-
-
+    fun fetchPokemonsList() = liveData {
+        emit(State.LoadingState)
+        try {
+            val response = pokedexRepository.fetchAllPokemons(153)
+            if (response.isSuccessful) {
+                response.body()?.let {
+                    emit(State.DataState(it.results))
+                } ?: run {
+                    emit(State.DataState(null))
+                }
+            }
+        } catch (e: Exception) {
+            emit(State.ErrorState(e))
+        }
+    }
 }
